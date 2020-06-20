1e2218a9dbe85f5aeae8092665170841ba94609d
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-20 13:34:34 -0500

    barebones header

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,32 @@
 import React from "react"
 import styled from "styled-components"
-import tw from "tailwind.macro"
 import logo from "../images/isaiah-logo.png"
-
-// logo
-// wrapper
-// hover effects
-// navbar: home, portfolio, about, contact, blog?
 
 const LogoWrapper = styled.a`
   display: inline-flex;
-  margin: auto 5px;
+  margin: 20px 20px;
   float: left;
 `
 
+const ChatButton = () => (
+  <button
+    className="border-black border-solid border-2 hover:border-white hover:bg-black text-black hover:text-white font-bold py-2 px-4 rounded inline-flex float-right m-10"
+  >
+    Let's chat
+  </button>
+)
+
 const Header = () => (
-  <LogoWrapper href="/">
-    <img
-      width="75"
-      src={logo}
-      alt="My Logo"
-  />
-  </LogoWrapper>
+  <>
+    <LogoWrapper href="/">
+      <img
+        width="75"
+        src={logo}
+        alt="My Logo"
+    />
+    </LogoWrapper>
+    <ChatButton />
+  </>
 )
 
 export default Header
