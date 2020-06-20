afdbd9ad5c36f8f90353a9c2a64e987ae886127e
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-20 13:38:50 -0500

    nada

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,7 @@
+import "./src/styles/globals.css"
+
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-// You can delete this file if you're not using it
-
-import "./src/styles/globals.css"
