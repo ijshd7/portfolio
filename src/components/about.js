0daa00d7e9b38e3a3357ead7a91c1e71fc117ff3
Isaiah <ijshd7@mail.missouri.edu>
2020-06-21 21:58:58 -0500

    adding basic fontawesome stuff to about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,7 @@
 import React from "react"
 import styled from "styled-components"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faCode, faLaptopCode } from '@fortawesome/free-solid-svg-icons'
 
 // web developer,software dev, cool dude
 
@@ -7,3 +9,28 @@ import styled from "styled-components"
 // technologies: html,css,js,react,gatsby,node,styled components,tailwindcss,postcss,jest, etc
 // categories: frameworks/libraries, testing, style, web apis, cross-platform
 // tools
+
+// columns, then column
+
+// need to restructure this to accomodate ^
+
+
+
+const About = () => (
+    <div className="text-center text-6xl">
+        <FontAwesomeIcon 
+            icon={faCode}
+            className="mx-24 text-red-500 hover:text-red-300"
+        />
+        <FontAwesomeIcon
+            icon={faLaptopCode}
+            className="mx-24 text-green-400 hover:text-green-300"
+        />
+        <FontAwesomeIcon
+            icon={faCoffee}
+            className="mx-24 text-blue-500 hover:text-blue-300"
+        />
+    </div>
+)
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import Header from "../components/header"
 import Hero from "../components/hero"
 import Introduction from "../components/introduction"
+import About from "../components/about"
 
 const IndexPage = () => (
   <>
     <Header />
     <Hero />
     <Introduction />
+    <About />
   </>
 )
 
