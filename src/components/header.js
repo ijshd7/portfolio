31ce6e8cc0e7b7f580c0c0aab792e55c8cf1eb55
Isaiah <ijshd7@mail.missouri.edu>
2020-06-22 17:14:48 -0500

    started messing with graphql inquiries

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import styled from "styled-components"
 import logo from "../images/isaiah-logo.png"
 import whitelogo from "../images/isaiah-logo-white.png"
@@ -80,6 +81,10 @@ const Header = () => (
     <LogoWrapper>
       <Logo />
     </LogoWrapper>
+    <Link className="text-3xl mx-2" to="blog" >Blog</Link>
+    <Link className="text-3xl mx-2" to="resources" >Resources</Link>
+    <Link className="text-3xl mx-2" to="resume" >Resume</Link>
+    <Link className="text-3xl mx-2" to="tutorials" >Tutorials</Link>
     <ChatButton />
   </HeaderWrapper>
 )
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,8 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 import ComingSoon from "../components/comingSoon"
 
 const Resume = () => (
-    <ComingSoon />
+    <>
+        <Link className="text-3xl mx-2" to="/" >Home</Link>
+        <Link className="text-3xl mx-2" to="/blog" >Blog</Link>
+        <Link className="text-3xl mx-2" to="/resources" >Resources</Link>
+        <Link className="text-3xl mx-2" to="/resume" >Resume</Link>
+        <Link className="text-3xl mx-2" to="/tutorials" >Tutorials</Link>
+        <ComingSoon />
+    </>
 )
 
 export default Resume
