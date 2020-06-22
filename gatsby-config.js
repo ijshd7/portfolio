b54ce7d4f8ceb48ab4e879520d774323900a61e5
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-22 18:56:15 -0500

    fixed ui error by adding styled components and tailwindcss plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-postcss`
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-tailwindcss`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
