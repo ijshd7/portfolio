64930122ed44db6657def220a5b61c9a69731235
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-21 19:05:01 -0500

    introduction section and added fontawesome imports

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,16 +1,26 @@
 import React from "react"
 import styled from "styled-components"
 
-// gradient 
-// introduction text
-
-const Gradient = styled.div`
-    
+const IntroWrapper = styled.div`
+    background: linear-gradient(45deg, #11A49B, #90FC8E);
+    height: 100%;
+    width: 100%;
 `
+
+const Text = () => (
+    <div className="text-center text-white p-16">
+        <h1 className="text-6xl mb-10">Nice to meet you.</h1>
+        <p className="text-lg inline-block m-auto pt-4 px-64">
+            A few years back I started delving into the world of web development and never looked back. I've learned so much and met so many awesome people, and I look forward to working with you. If you already know me, what do you think of my site? And if you don't know me, here is what I do
+        </p>
+    </div>
+)
 
 
 const Introduction = () => (
-    <Gradient />
+    <IntroWrapper>
+        <Text />
+    </IntroWrapper>
 )
 
 export default Introduction
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,4 +11,5 @@ const IndexPage = () => (
   </>
 )
 
+
 export default IndexPage
