f18db0f16b967d7ce067e9a08ccbf9382a9314c9
Isaiah <ijshd7@mail.missouri.edu>
2020-06-23 21:24:11 -0500

    adding another tester

---
+++ src/components/gridExtension.js
@@ -0,0 +1,49 @@
+import React from "react"
+import styled from "styled-components"
+
+const Row = styled.div`
+    &::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+`
+
+function getWidthString(span) {
+    if (!span) return;
+
+    let width = span / 12 * 100;
+    return `width: ${width}%;`
+}
+
+const Column = styled.div`
+    float: left;
+    ${({ xs }) => (xs ? getWidthString(xs) : "width: 100%")};
+
+    @media only screen and (min-width: 768px) {
+        ${({ sm }) => sm && getWidthString(sm)};
+    }
+
+    @media only screen and (min-width: 992px) {
+        ${({ md }) => md && getWidthString(md)};
+    }
+
+    @media only screen and (min-width: 1200px) {
+        ${({ lg }) => lg && getWidthString(lg)};
+    }
+`
+
+export default function GridExtension(props) {
+    return (
+        <div>
+            <Row>
+                <Column xs="12" sm="6" md="8">
+                    md: 8 - sm: 6 -xs: 12
+                </Column>
+                <Column xs="6" md="4">
+                    md: 4 - xs: 6
+                </Column>
+            </Row>
+        </div>
+    )
+}
---
+++ src/pages/test2.js
@@ -0,0 +1,9 @@
+import React from "react"
+import GridExtension from "../components/gridExtension"
+
+const Test2 = () => (
+    <GridExtension />
+)
+
+
+export default Test2
