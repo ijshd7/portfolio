5ca6ba0771900ce71f256a1aac3578fad1865507
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-24 20:49:54 -0500

    making progress on responsive header

--- src/components/header.js
+++ src/components/header.js
@@ -6,12 +6,6 @@ import whitelogo from "../images/isaiah-logo-white.png"
 
 const createReactClass = require('create-react-class');
 
-const LogoWrapper = styled.div`
-  display: inline-flex;
-  margin: 25px 25px;
-  float: left;
-`
-
 const Logo = createReactClass({
   getInitialState: function () {
     return {
@@ -52,43 +46,63 @@ const Logo = createReactClass({
       </a>
     )
   }
-});
+})
+
+const Links = () => (
+  <>
+    <Link className="text-3xl mx-2" to="blog" >Blog</Link>
+    <Link className="text-3xl mx-2" to="resources" >Resources</Link>
+    <Link className="text-3xl mx-2" to="resume" >Resume</Link>
+    <Link className="text-3xl mx-2" to="tutorials" >Tutorials</Link>
+  </>
+)
+
+const Row = styled.div`
+    &::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+`
+
+const Column = styled.div`
+    float: left;
+    width: 100%;
+
+    @media only screen and (min-width: 768px) {
+        width: ${props => (props.span ? props.span / 12 * 100 : "8.33")}%;
+    }
+`
+
+function Grid(props) {
+    return (
+        <div>
+            <Row>
+                <Column className="mt-3" span="3">
+                  <Logo />
+                </Column>
+                <Column className="mt-3" span="6">
+                  <Links />
+                </Column>
+                <Column className="mt-3" span="3">
+                  <ChatButton />
+                </Column>
+            </Row>
+        </div>
+    )
+}
 
 const ChatButton = () => (
   <button
-    className="border-black border-solid border-2 hover:border-white hover:bg-black text-black hover:text-white font-bold py-2 px-4 rounded-full inline-flex float-right mr-12"
+    className="border-black border-solid border-2 hover:border-white hover:bg-black text-black hover:text-white font-bold py-2 px-4 rounded-full"
   >
     Let's Chat
   </button>
 )
 
-const HeaderWrapper = styled.nav`
-  padding: 1.2rem 0;
-  box-shadow: none;
-  z-index: auto;
-  background-color: white;
-  position: relative;
-  margin: 0;
-  padding: 0;
-  border: 0;
-  vertical-align: baseline;
-  text-rendering: auto;
-  text-size-adjust: 100%;
-`
 
 const Header = () => (
-  <HeaderWrapper>
-    <LogoWrapper>
-      <Logo />
-    </LogoWrapper>
-    <div className="text-center">
-      <Link className="text-3xl mx-2" to="blog" >Blog</Link>
-      <Link className="text-3xl mx-2" to="resources" >Resources</Link>
-      <Link className="text-3xl mx-2" to="resume" >Resume</Link>
-      <Link className="text-3xl mx-2" to="tutorials" >Tutorials</Link>
-    </div>
-    <ChatButton />
-  </HeaderWrapper>
+    <Grid />
 )
 
 export default Header
