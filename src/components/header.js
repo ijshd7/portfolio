8147e23bb179882ee004caae1eb9025a480f82ff
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-25 20:55:49 -0500

    locking in height for mobile

--- src/components/header.js
+++ src/components/header.js
@@ -32,7 +32,7 @@ const Logo = createReactClass({
   },
 
   render: function () {
-    let className = this.state.hovered ? 'bg-black border-white border-4 rounded-md w-16' : 'border-black border-4 rounded-md w-16';
+    let className = this.state.hovered ? 'bg-black border-white border-4 rounded-md w-16 h-16' : 'border-black border-4 rounded-md w-16 h-16';
     let src = this.state.hovered ? whitelogo : logo;
     return (
       <Link
