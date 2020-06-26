ba56054b49aecff31108b3f9855ef1d56b6f30cf
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-25 21:37:50 -0500

    making introduction section responsive

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -3,14 +3,15 @@ import styled from "styled-components"
 
 const IntroWrapper = styled.div`
     background: linear-gradient(45deg, #11A49B, #90FC8E);
+    display: flex;
     height: 100%;
     width: 100%;
 `
 
 const Text = () => (
     <div className="text-center text-white p-16">
-        <h1 className="text-6xl mb-10">Nice to meet you.</h1>
-        <p className="text-lg inline-block m-auto pt-4 px-64">
+        <h1 style={{fontSize: "8vh"}} className="mb-10">Nice to meet you.</h1>
+        <p className="text-base inline-block m-auto pt-4 px-64">
             A few years ago I started delving into the world of web development and never looked back. I've learned so much and met so many awesome people, and I look forward to working with you. If you already know me, what do you think of my site? And if you don't know me, here is what I do
         </p>
     </div>
