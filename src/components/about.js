759f22475f0d8331ed34062e0bbd535ab2bcfc9d
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-26 20:20:48 -0500

    made about section responsive. pushing to test on mobile

--- src/components/about.js
+++ src/components/about.js
@@ -8,78 +8,85 @@ import { faCode, faLaptopCode, faCoffee } from '@fortawesome/free-solid-svg-icon
 // icons responsive
 // clean up some text
 
-const Columns = styled.div`
-    display: flex;
-    box-sizing: border-box;
-    margin-top: -0.75rem;
-    margin-bottom: 0.75rem;
-    margin-left: -0.75rem;
-    margin-right: -0.75rem;
-    min-width: 100%;
-    padding: 30px 0;
+const Row = styled.div`
+    &::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
 `
+
+function getWidthString(span) {
+  if (!span) return;
+
+  let width = span / 12 * 100;
+  return `width: ${width}%;`;
+}
 
 const Column = styled.div`
-    display: block;
-    box-sizing: inherit;
-    flex-basis: 0;
-    flex-grow: 1;
-    flex-shrink: 1;
-    padding: 10.5px;
-    text-align: center;
+    float: left;
+    ${({ xs }) => (xs ? getWidthString(xs) : "width: 100%")};
+
+    @media only screen and (min-width: 768px) {
+      ${({ sm }) => sm && getWidthString(sm)};
+    }
+
+    @media only screen and (min-width: 992px) {
+      ${({ md }) => md && getWidthString(md)};
+    }
+
+    @media only screen and (min-width: 1200px) {
+      ${({ lg }) => lg && getWidthString(lg)};
+    }
 `
 
-const Content = styled.div`
-    box-sizing: inherit;
-    text-align: center;
-`
-
-const Header = styled.h1`
-    font-weight: bold;
-    margin-top: 1.3333em;
-    margin-bottom: .6666em;
-`
+function AboutGrid(props) {
+    return (
+        <div className="font-mono antialiased">
+            <Row>
+                <Column xs="12" sm="4" md="6" lg="3" className="mt-4 text-center">
+                    <FontAwesomeIcon
+                        icon={faCode}
+                        className="text-red-500 hover:text-red-300"
+                        style={{fontSize: "8vh"}}
+                    />
+                    <h1 className="mt-4" style={{fontSize: "3vh"}}>Developer</h1>
+                    <p className="mt-2" style={{fontSize: "2vh"}}>I build websites, web apps, and software</p>
+                    <br />
+                    <p style={{fontSize: "3vh"}} className="text-red-500 mt-4">Core Technologies:</p>
+                    <p style={{fontSize: "2vh"}} className="mt-2 mx-10">HTML, CSS, URLs, HTTP, JavaScript, JSON, DOM, Web APIs, WCAG, and ARIA</p>
+                </Column>
+                <Column xs="12" sm="4" md="6" lg="6" className="mt-4 text-center">
+                    <FontAwesomeIcon
+                        icon={faLaptopCode}
+                        className="text-green-400 hover:text-green-300"
+                        style={{fontSize: "8vh"}}
+                    />
+                    <h1 className="mt-4" style={{fontSize: "3vh"}}>Lifelong Learner</h1>
+                    <p className="mt-2" style={{fontSize: "2vh"}}>I love learning about the ecosystem of the web, and all the cool technologies it entails</p>
+                    <br />
+                    <p style={{fontSize: "3vh"}} className="text-green-400 mt-4">Libraries, Frameworks, Tools:</p>
+                    <p style={{fontSize: "2vh"}} className="mt-2 mx-10">React, React Native, Gatsby, Tailwindcss, JSX, Node.js, Data Formats, REST APIs, Package Managers, SPA/PWAs, Linting, and so many more </p>
+                </Column>
+                <Column xs="12" sm="4" md="6" lg="3" className="mt-4 text-center">
+                    <FontAwesomeIcon
+                        icon={faCoffee}
+                        className="text-blue-500 hover:text-blue-300"
+                        style={{fontSize: "8vh"}}
+                    />
+                    <h1 className="mt-4" style={{fontSize: "3vh"}}>Cool Dude</h1>
+                    <p className="mt-2" style={{fontSize: "2vh"}}>I truly enjoy meeting people from all walks of life and growing as a person</p>
+                    <br />
+                    <p style={{fontSize: "3vh"}} className="text-blue-500 mt-4">Experiences I draw from:</p>
+                    <p style={{fontSize: "2vh"}} className="mt-2 mx-10">A career in healthcare, a burning interest in technology, and wanting the best for every person that I meet</p>
+                </Column>
+            </Row>
+        </div>
+    )
+}
 
 const About = () => (
-    <Columns className="font-mono antialiased">
-        <Column>
-            <Content>
-                <FontAwesomeIcon
-                    icon={faCode}
-                    className="text-6xl text-red-500 hover:text-red-300"
-                />
-                <Header className="text-3xl">Developer</Header>
-                <p>I build websites, web apps, and software</p>
-                <br />
-                <p className="text-xl text-red-500 mt-8">Core Technologies:</p>
-                <p className="mt-2 mx-10">HTML, CSS, URLs, HTTP, JavaScript, JSON, DOM, Web APIs, WCAG, and ARIA</p>
-            </Content>
-        </Column>
-        <Column>
-            <Content>
-                <FontAwesomeIcon
-                    icon={faLaptopCode}
-                    className="text-6xl text-green-400 hover:text-green-300"
-                />
-                <Header className="text-3xl">Lifelong Learner</Header>
-                <p>I love learning about the ecosystem of the web, and all the cool technologies it entails</p>
-                <p className="text-xl text-green-400 mt-8">Libraries, Frameworks, Tools:</p>
-                <p className="mt-2 mx-10">React, React Native, Gatsby, Tailwindcss, JSX, Node.js, Data Formats, REST APIs, Package Managers, SPA/PWAs, Linting, and so many more </p>
-            </Content>
-        </Column>
-        <Column>
-            <Content>
-                <FontAwesomeIcon
-                    icon={faCoffee}
-                    className="text-6xl text-blue-500 hover:text-blue-300"
-                />
-                <Header className="text-3xl">Cool Dude</Header>
-                <p>I truly enjoy meeting people from all walks of life and growing as a person</p>
-                <p className="text-xl text-blue-500 mt-8">Experiences I draw from:</p>
-                <p className="mt-2 mx-10">A career in healthcare, a passion for understanding what makes things the way that they are, and a burning interest in technology</p>
-            </Content>
-        </Column>
-    </Columns>
+    <AboutGrid />
 )
 
 export default About
