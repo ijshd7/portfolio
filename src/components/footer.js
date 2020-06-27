0c069a4f4ec9d50cd3547a473c72c69f90e706fe
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-27 00:45:07 -0500

    touched up the footer, going to test on mobile to see how it looks

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,41 +10,74 @@ import logo from "../images/isaiah-logo-white.png"
 library.add(fab, faEnvelope)
 
 const FooterWrapper = styled.div`
-    background: linear-gradient(45deg, #11A49B, #90FC8E);
-    height: 100%;
+    background: linear-gradient(45deg, #11A49B, #90FC8E) no-repeat center top;
+    background-size: contain;
+    display: inline-block;
+    position: relative;
+    height: auto;
     width: 100%;
+    text-align: center;
 `
 
 const Logo = () => (
-    <div className="text-center pt-10 mb-10">
-        <Link to="/">
-            <img className="inline-block mr-auto ml-auto" src={logo} alt="Cat logo" width="100"/>
-        </Link>
-    </div>
+    <Link to="/">
+        <span className="inline-flex">
+            <img className="mx-8 w-8 h-8" src={logo} alt="Cat logo" />
+        </span>
+    </Link>
 )
 
 const Footer = () => (
-    <FooterWrapper>
-        <Logo />
-        <div className="text-center">
-            <FontAwesomeIcon
-                icon={['fab', 'github']}
-                className=" text-5xl mx-8 text-white hover:text-green-400 hover:bg-white rounded-full h-8 w-8"
-            />
-            <FontAwesomeIcon
-                icon={['fab', 'linkedin-in']}
-                className=" text-5xl mx-8 text-white hover:text-green-400 hover:bg-white rounded-full h-8 w-8"
-            />
-            <FontAwesomeIcon
-                icon={['fab', 'codepen']}
-                className=" text-5xl mx-8 text-white hover:text-green-400 hover:bg-white rounded-full h-8 w-8"
-            />
-            <FontAwesomeIcon
-                icon="envelope"
-                className=" text-5xl mx-8 text-white hover:text-green-400 hover:bg-white rounded-full h-8 w-8"
-            />
+    <FooterWrapper className="py-4">
+        <div>
+            <a
+                href="https://github.com/ijshd7"
+                target="_blank" 
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon
+                    icon={['fab', 'github']}
+                    className="mx-8 text-white h-8 w-8"
+                />
+            </a>
+            <a
+                href="https://www.linkedin.com/in/isaiah-sherrill-ab58b970"
+                target="_blank" 
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon
+                    icon={['fab', 'linkedin-in']}
+                    className="mx-8 text-white h-8 w-8"
+                />
+            </a>
+            <a
+                href="https://codepen.io/ijshd7"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon
+                    icon={['fab', 'codepen']}
+                    className="mx-8 text-white h-8 w-8"
+                />
+            </a>
+            <a
+                href="mailto:ijshd7@mail.missouri.edu"
+                target="_blank" 
+                rel="noreferrer"
+            >
+                <FontAwesomeIcon
+                    icon="envelope"
+                    className="mx-8 text-white h-8 w-8"
+                />
+            </a>
+            <Logo />
         </div>
-        <p className="text-3xl text-center text-white mt-10 pb-4 font-mono antialiased">Created with love by Isaiah 2020</p>
+        <p 
+            style={{fontSize: "4vh"}}
+            className="text-center text-white mt-10 pb-4 font-mono antialiased"
+        >
+            Created with love by Isaiah 2020
+        </p>
     </FooterWrapper>
 )
 
