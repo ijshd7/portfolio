3c5d0668d4e2181ef798c40b815a90bd7ad1a6cd
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-06-28 14:56:31 -0500

    removing boilerplate

---
+++ src/pages/dropdown.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "styled-components"
+import { CSSTransition } from "react-transition-group"
+
+
+
+export default function Test() {
+    return (
+      <>
+      </>
+    );
+}
