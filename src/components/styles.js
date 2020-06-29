391a635debd280546bc24ea8b33cb604af0da1d4
Isaiah <ijshd7@mail.missouri.edu>
2020-06-28 22:03:56 -0500

    chippin away..

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
-import styled, { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle} from 'styled-components'
 import { CSSTransition } from 'react-transition-group'
 import { ReactComponent as BellIcon } from '../images/bell.svg'
 import { ReactComponent as MessengerIcon } from '../images/messenger.svg'
@@ -9,3 +9,24 @@ import { ReactComponent as CogIcon } from '../images/cog.svg'
 import { ReactComponent as ChevronIcon } from '../images/chevron.svg'
 import { ReactComponent as ArrowIcon } from '../images/arrow.svg'
 import { ReactComponent as BoltIcon } from '../images/bolt.svg'
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --bg:  #242526;
+    --bg-accent: #484a4d;
+    --text-color: #dadce1;
+    --nav-size: 60px;
+    --border: 1px solid #474a4d;
+    --border-radius: 8px;
+    --speed: 500ms; 
+  }
+`
+
+const App = ({ children }) => (
+    <>
+        <GlobalStyles />
+        {children}
+    </>
+)
+
+
