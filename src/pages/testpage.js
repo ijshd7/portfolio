eca55fa769394085fd597f658f4209dc6fcc5492
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-07-05 19:10:33 -0500

    adjusting test page

--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -1,8 +1,9 @@
 import React from "react"
-import Navbar from "../components/navbar"
 
 export default function TestPage() {
     return (
-        <Navbar />
+        <>
+            <h1>TEST PAGE LOL</h1>
+        </>
     );
 }
