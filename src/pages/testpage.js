a4e282e55055669d126f3139976b8b0ffe801c33
Isaiah <ijshd7@mail.missouri.edu>
2020-07-06 22:51:01 -0500

    adding commits, small commit

--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -1,5 +1,9 @@
 import React from "react"
 
+// need to add projects, build testimonial section
+// use grid layout inspiration and possibly change theming to dark
+// AND/OR add dark/light theme
+
 export default function TestPage() {
     return (
         <>
