766dd22756719db64e85f1322924e5c19e0031b3
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-07-12 11:03:53 -0500

    working on grid vs flex for projects

--- src/pages/testpage2.js
+++ src/pages/testpage2.js
@@ -2,14 +2,25 @@ import React from "react"
 import styled from "styled-components"
 import Layout from "../components/layout"
 import movet from "../images/movetscreenshot.png"
-import rise from "../images/risescreenshot.png"
+import movetlogo from "../images/movetlogo.png"
+import movetiphone1 from "../images/movetiphone.png"
+import movetiphone2 from "../images/movetiphone2.png"
+import riselogo from "../images/riselogo.png"
+import rise1 from "../images/risescreenshot.png"
+import rise2 from "../images/risescreenshot1.png"
+import rise3 from "../images/risescreenshot2.png"
+import rise4 from "../images/risescreenshot3.png"
+
+// carousel for rise
+// hover effect for movet
+// other projects link
 
 const Wrapper = styled.div`
     margin-top: 5rem;
     margin-bottom: 5rem;
     display: grid;
+    align-items: center;
     gap: 1.25rem;
-    grid-template-columns: repeat(auto-fit, minmax(250px,500px));
     justify-content: space-evenly;
 `
 
@@ -19,15 +30,9 @@ const ImgWrapper = styled.div`
     font-weight: 300;
 `
 
-const ImageOne = () => (
+const Image = ({source}) => (
     <ImgWrapper>
-        <img alt="pic" src={movet} />
-    </ImgWrapper>
-)
-
-const ImageTwo = () => (
-    <ImgWrapper>
-        <img alt="pic" src={rise} />
+        <img alt="pic" width="250px" src={source} />
     </ImgWrapper>
 )
 
@@ -35,14 +40,15 @@ export default function TestPage() {
     return (
         <Layout>
             <Wrapper>
-                <ImageOne />
-                <ImageTwo />
-                <ImageOne />
-                <ImageTwo />
-                <ImageOne />
-                <ImageTwo />
-                <ImageOne />
-                <ImageTwo />
+                <Image source={movetlogo} />
+                <Image source={riselogo} />
+                <Image source={movet} />
+                <Image source={rise1} />
+                <Image source={movetiphone1} />
+                <Image source={rise2} />
+                <Image source={movetiphone2} />
+                <Image source={rise3} />
+                <Image source={rise4} />
             </Wrapper>
         </Layout>
     );
