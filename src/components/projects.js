5d6ad072a7905426cf3fadaeae9eb301a936fe14
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-07-25 12:35:41 -0500

    adding media query to test on mobile

--- src/components/projects.js
+++ src/components/projects.js
@@ -73,8 +73,10 @@ const Movet = ({source1, source2}) => (
 )
 
 const RiseWrapper = styled.div`
-    margin-top: 5rem;
-    margin-right: 8rem;
+    @media only screen and (min-width: 768px) {
+        margin-top: 5rem;
+        margin-right: 8rem;
+    }
     max-width: 550px;
 `
 
