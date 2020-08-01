0f416b72c29fec31569b8fea47d9a3899b73da90
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-08-01 00:41:36 -0500

    got starter contact form done. deploying to check on mobile

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -32,56 +32,68 @@ const ContactForm = () => {
         });
     };
     return (
-        <div className="text-center m-auto w-3/5">
-            <h3>Let's Chat!</h3>
+      <>
+        <h1 className="text-center font-sans text-lg font-bold mt-4 mb-6">Let's Chat!</h1>
+        <div className="text-center m-auto flex justify-center align-middle w-3/5">
             <form
               className="w-full max-w-sm"
               onSubmit={handleOnSubmit}
             >
-              <div className="">
-                <label
-                  for="exampleInputEmail1"
-                  required="required"
-                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                >
-                  Email address
-                </label>
-              </div>
-              <div className="">
-                  <input
-                    type="email"
-                    name="email"
-                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter email"
-                  />
-              </div>
-              <div className="mb-6">
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
                   <label
-                    for="exampleInputName"
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                  >
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                    id="exampleInputName"
-                    placeholder="Enter your name"
+                    for="exampleInputEmail1"
                     required="required"
-                  />
+                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  >
+                    Email address
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                    <input
+                      type="email"
+                      name="email"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter email"
+                    />
+                </div>
               </div>
-              <div className="form-group">
-                  <label for="exampleFormControlSelect1">What's up?</label>
-                  <select className="form-control" id="exampleFormControlSelect1" name="platform" required="required">
-                  <option>Would like to chat</option>
-                  <option>Just saying hi</option>
-                  <option>I like your site!</option>
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                    <label
+                      for="exampleInputName"
+                      className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                    >
+                      Name
+                    </label>
+                </div>
+                <div className="md:w-2/3">
+                    <input
+                      type="text"
+                      name="name"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      id="exampleInputName"
+                      placeholder="Enter your name"
+                      required="required"
+                    />
+                </div>
+              </div>
+              <div className="">
+                  <label
+                    className="font-sans text-lg font-bold"
+                    for="exampleFormControlSelect1"
+                  >
+                    What should we talk about?
+                  </label>
+                  <select className="text-green-500" id="exampleFormControlSelect1" name="platform" required="required">
+                    <option>Web Development</option>
+                    <option>Suggestions</option>
+                    <option>I like your site!</option>
                   </select>
               </div>
-              <button type="submit" className="my-4 bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded"  disabled={serverState.submitting}>
+              <button type="submit" className="mt-4 mb-6 text-green-400 bg-white border border-green-400 hover:border-transparent hover:bg-green-500 hover:text-white font-bold py-3 px-6 rounded-full"  disabled={serverState.submitting}>
                   Submit
               </button>
               {serverState.status && (
@@ -91,6 +103,7 @@ const ContactForm = () => {
               )}
             </form>
         </div>
+      </>
     );
   };
   
