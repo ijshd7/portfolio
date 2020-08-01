c373cf7681f017bb52d4bcbdae3de7151deb31de
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-08-01 11:21:24 -0500

    more tweaks to the form for now

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -44,7 +44,7 @@ const ContactForm = () => {
                   <label
                     for="exampleInputEmail1"
                     required="required"
-                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                    className="block text-green-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
                   >
                     Email address
                   </label>
@@ -53,7 +53,7 @@ const ContactForm = () => {
                     <input
                       type="email"
                       name="email"
-                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
                       id="exampleInputEmail1"
                       aria-describedby="emailHelp"
                       placeholder="Enter email"
@@ -64,7 +64,7 @@ const ContactForm = () => {
                 <div className="md:w-1/3">
                     <label
                       for="exampleInputName"
-                      className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                      className="block text-green-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
                     >
                       Name
                     </label>
@@ -73,7 +73,7 @@ const ContactForm = () => {
                     <input
                       type="text"
                       name="name"
-                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
                       id="exampleInputName"
                       placeholder="Enter your name"
                       required="required"
@@ -93,7 +93,7 @@ const ContactForm = () => {
                     <option>I like your site!</option>
                   </select>
               </div>
-              <button type="submit" className="mt-4 mb-6 text-green-400 bg-white border border-green-400 hover:border-transparent hover:bg-green-500 hover:text-white font-bold py-3 px-6 rounded-full"  disabled={serverState.submitting}>
+              <button type="submit" className="mt-4 mb-8 text-lg text-green-400 bg-white border border-green-400 hover:border-transparent hover:bg-green-500 hover:text-white font-bold py-2 px-4 rounded-full"  disabled={serverState.submitting}>
                   Submit
               </button>
               {serverState.status && (
