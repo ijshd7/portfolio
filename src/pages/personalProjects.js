44387f323ad57fb7b2bbaf103381f269f7620986
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-01-16 19:18:13 -0600

    adding a few starter files for things

---
+++ src/pages/personalProjects.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function PersonalProjectsPage() {
+    return (
+      <>
+        <h1>Hello! This is my personal projects page!</h1>
+      </>
+    );
+  }
---
+++ src/pages/professionalProjects.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function ProfessionalProjectsPage() {
+    return (
+      <>
+        <h1>Hello! This is my professional projects page!</h1>
+      </>
+    );
+  }
