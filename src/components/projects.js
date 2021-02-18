751fe892c140444a48d89896788f731e661f389c
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-02-17 19:12:47 -0600

    current projects laid out

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,6 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import floating1 from '../images/floatingstl1.png';
 import floatingLogo from '../images/float-logo-teal.png';
+import dogwood1 from '../images/dogwood1.png';
+import dogwoodLogo from '../images/dogwood-advisors-vertical-logo.png';
+import rise1 from '../images/riseCarousel3.png';
+import riseLogo from '../images/rise-logo-white.png';
+import coach1 from '../images/mycoachkevin1.png';
+import coachLogo from '../images/logo.png';
+import movet1 from '../images/movetiphone.png';
+import movetLogo from '../images/movetLogoWithIcon.png'
 
 const ProjectsWrapper = styled.section`
     background: linear-gradient(45deg, #11A49B, #90FC8E) no-repeat center top;
@@ -18,8 +26,28 @@ const Projects = () => (
         <div className="my-16 text-center text-white font-mono antialiased">
             <h1 style={{fontSize: "8vh"}} className="mb-10">PROFESSIONAL PROJECTS</h1>
             <div className="flex mb-2">
-                <img className="w-1/3 mx-auto" src={floating1} alt="float screenshot" />
-                <img className="w-1/3 mx-auto" src={floatingLogo} alt="float logo" />
+                <div className="w-1/4 mx-auto">
+                    <img className="" src={floating1} alt="Floating STL screenshot"/>
+                    <img className="mt-2 w-1/2 mx-auto" src={floatingLogo} alt="Floating STL logo"/>
+                </div>
+                <div className="w-1/4 mx-auto">
+                    <img className="" src={dogwood1} alt="Dogwood Compliance screenshot"/>
+                    <img className="mt-2 mx-auto" src={dogwoodLogo} alt="Dogwood Compliance logo"/>
+                </div>
+                <div className="w-1/4 mx-auto">
+                    <img className="" src={rise1} alt="Rise Float Gathering screenshot"/>
+                    <img className="mt-2 w-1/2 mx-auto" src={riseLogo} alt="Rise Float Gathering logo"/>
+                </div>
+            </div>
+            <div className="flex mt-6">
+                <div className="w-1/4 mx-auto">
+                    <img className="" src={coach1} alt="Elevated Coaching screenshot"/>
+                    <img className="mt-2 w-1/2 mx-auto" src={coachLogo} alt="Elevated Coaching logo"/>
+                </div>
+                <div className="w-1/4 mx-auto">
+                    <img className="h-1/2 mx-auto" src={movet1} alt="Movet screenshot"/>
+                    <img className="mt-2 mx-auto" src={movetLogo} alt="Movet logo"/>
+                </div>
             </div>
         </div>
     </ProjectsWrapper>
