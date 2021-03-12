62668e53266811c9cbf0b00d0a7f28ccfbfb10e5
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-03-11 23:37:22 -0600

    minor changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import avatarJpg from "../images/southParkAvatar.jpg";
-import avatarWebp from "../images/southParkAvatar.webp";
 import computerPng from "../images/computer.png";
 import computerWebp from "../images/computer.webp";
+import logoPng from '../images/isaiah-logo.png';
+import logoWebp from '../images/isaiah-logo.webp';
 
 const HeroWrapper = styled.section`
     background-color: white;
@@ -22,17 +22,16 @@ const HeroWrapper = styled.section`
     max-width: 100%;
     font-family: 'Monoton', cursive;
 `
-
 const Hero = () => (
     <HeroWrapper>
-        <h1 style={{fontSize: "8vh"}}>Hi, my name is Isaiah!</h1>
-        <p className="mt-2" style={{fontSize: "5vh"}}>I build things</p>
-        <picture className="my-16 mx-auto block h-auto w-1/4">
-            <source type="image/webp" srcSet={avatarWebp}/>
-            <source type="image/jpg" srcSet={avatarJpg}/>
-            <img src={avatarJpg} alt="My avatar"/>
+        <h1 className="z-10 text-black" style={{fontSize: "8vh"}}>Hi, my name is Isaiah!</h1>
+        <p className="z-10 mt-2 mb-6 sm:mb-12 lg:mb-12 text-black" style={{fontSize: "5vh"}}>I build things</p>
+        <picture className="z-10 -mb-24 lg:-mb-64 mx-auto block h-auto w-1/12">
+            <source type="image/webp" srcSet={logoWebp}/>
+            <source type="image/jpg" srcSet={logoPng}/>
+            <img src={logoPng} alt="My avatar"/>
         </picture>
-        <picture className="mt-12 mx-auto block h-auto w-1/2">
+        <picture className="mt-10 mx-auto block h-auto w-1/2">
             <source type="image/webp" srcSet={computerWebp}/>
             <source type="image/png" srcSet={computerPng}/>
             <img src={computerPng} alt="Computer"/>
