f192ea6b8ab471182d9eb35401c49b4261ac6afd
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-03-13 23:52:35 -0600

    another test push

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,7 +3,7 @@ import React from "react";
 const TestPage = () => {
     return (
         <section className="h-screen w-full flex">
-            <h1 className="pt-36 mx-auto">HELLO WORLD LOL</h1>
+            <h1 className="pt-36 mx-auto text-yellow-500">HELLO WORLD LOL</h1>
         </section>
     );
 }
