950e3b541ed43a4bdb331bd42e8c08e2b3a7b48a
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-03-13 23:43:40 -0600

    starting page to redo portfolio

---
+++ src/pages/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestPage = () => {
+    return (
+        <section className="h-screen w-full flex">
+            <h1 className="pt-36 mx-auto">HELLO WORLD LOL</h1>
+        </section>
+    );
+}
+
+export default TestPage;
