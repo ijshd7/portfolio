1a6cafeb709483cf0c01d5d6e5f3a6ff0d527dbf
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-03-25 00:56:02 -0500

    small addition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-// https://dribbble.com/shots/15291361-Toby-Brancher-Home-Desktop/attachments/7045604?mode=media
-// https://www.dejan.works/
+// // https://dribbble.com/shots/15291361-Toby-Brancher-Home-Desktop/attachments/7045604?mode=media
+// // https://www.dejan.works/
 
-const IndexPage = () => {
+const NewIndexPage = () => {
   return (
     <section className="h-screen w-full flex flex-col justify-center bg-gray-900 font-serif">
       <div className="text-left text-6xl lg:text-8xl ml-4 lg:ml-48">
@@ -11,7 +11,9 @@ const IndexPage = () => {
         <h1 className="text-green-400 uppercase my-4">I'm</h1>
         <h1 className="text-green-400 uppercase">Isaiah</h1>
       </div>
+      <p className="text-center text-white">Tap anywhere</p>
+
     </section>
   );
 }
-export default IndexPage;
+export default NewIndexPage;
