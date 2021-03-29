fa2e527a79f05879aae948fe97dcec2566dbb3be
Isaiah Sherrill <me@isaiahsherrill.com>
2021-03-28 19:26:33 -0500

    started initial projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectsPage = () => {
+    return (
+        <section className="h-screen w-full flex">
+            <h1 className="pt-36 mx-auto text-yellow-500">HELLO WORLD LOL</h1>
+        </section>
+    );
+}
+
+export default ProjectsPage;
