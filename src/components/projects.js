836aaa25eca9ef2bdf75b0e487a93ec28614f65d
Isaiah Sherrill <me@isaiahsherrill.com>
2021-04-04 15:42:54 -0500

    minor ui tweaks for now

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
-import styled from "styled-components";
 import { Dialog } from '@reach/dialog';
 import "@reach/dialog/styles.css";
 import floating1 from '../images/floatingstl1.png';
@@ -14,15 +13,6 @@ import coachLogo from '../images/logo.png';
 import movet1 from '../images/movetiphone.png';
 import movetLogo from '../images/movetLogoWithIcon.png'
 
-const ProjectsWrapper = styled.section`
-    background-color: #111827;
-    background-size: contain;
-    display: inline-block;
-    position: relative;
-    height: auto;
-    width: 100%;
-`;
-
 const ProjectModal = ({ contributions, technologies }) => {
     const [showProject, setShowProject] = useState(false);
     const open = () => setShowProject(true);
@@ -31,20 +21,38 @@ const ProjectModal = ({ contributions, technologies }) => {
     return (
         <div className="my-2">
             <button className="text-white focus:outline-none cursor-pointer no-underline hover:text-green-400 transition-all duration-300 ease-linear" onClick={open}>Learn more</button>
-            <Dialog className="flex flex-col text-center" isOpen={showProject} onDismiss={close}>
-                <p className="text-black"><span className="underline text-green-400">Contributions:</span> {contributions}</p>
-                <p className="text-black"><span className="underline text-green-400">Technologies:</span> {technologies}</p>
-                <button className="text-gray-900 hover:text-red-300 focus:outline-none cursor-pointer underline" onClick={close}>close</button>
+            <Dialog
+                className="flex flex-col text-center"
+                isOpen={showProject}
+                onDismiss={close}
+                aria-label="Dialog"
+                role="alertdialog"
+            >
+                <p className="text-black text-sm lg:text-lg"><span className="underline text-green-400">Contributions:</span> {contributions}</p>
+                <p className="text-black text-sm lg:text-lg"><span className="underline text-green-400">Technologies:</span> {technologies}</p>
+                <button className="text-green-400 hover:text-red-300 focus:outline-none cursor-pointer underline mt-2" onClick={close}>close</button>
             </Dialog>
         </div>
     );
 }
 
 const Projects = () => (
-    <ProjectsWrapper>
+    <section className="h-auto w-full flex flex-col bg-gray-900 font-serif">
+        <div className="text-center mt-8 lg:hidden">
+            <Link to="/" className="underline text-center text-white hover:text-green-400 transition-all duration-300 ease-linear lg:hidden">Home</Link>
+        </div>
+        <div className="hidden lg:inline-flex space-x-20 pt-8 mx-8">
+            <div className="flex-1 group">
+            <hr className="border-gray-400 group-hover:border-green-400 transition-all duration-300 ease-linear"/>
+            <Link to="/" className="text-white hover:text-green-300 transition-all duration-300 ease-linear">Home</Link>
+            </div>
+            <div className="flex-1 group">
+            <hr className="border-gray-400 group-hover:border-green-400 transition-all duration-300 ease-linear"/>
+            <Link to="/contact/" className="text-white hover:text-green-300 transition-all duration-300 ease-linear">Contact</Link>
+            </div>
+        </div>
         <div className="mt-6 text-center text-white font-mono antialiased">
-            <h1 className="mx-auto text-3xl">PROFESSIONAL PROJECTS</h1>
-            <Link to="/" className="underline text-white hover:text-green-400 transition-all duration-300 ease-linear">back</Link>
+            <h1 className="mx-auto mt-4 pb-4 text-3xl underline">PROFESSIONAL PROJECTS</h1>
             <div className="flex-col lg:flex lg:flex-row mt-4 mb-4">
                 <div className="w-2/3 mb-6 lg:w-1/4 mx-auto">
                     <div className="relative">
@@ -100,7 +108,7 @@ const Projects = () => (
                     <div className="relative">
                         <img className="sm:h-1/3 lg:h-2/3 mx-auto rounded-lg" src={movet1} alt="Movet screenshot"/>
                         <ProjectModal
-                            contributions="Added new functionality/integrations, dependency updates, debugging"
+                            contributions="Added new functionality, dependency updates, debugging"
                             technologies="React Native, Android Studio, XCode, Firebase"
                         />
                     </div>
@@ -110,7 +118,7 @@ const Projects = () => (
                 </div>
             </div>
         </div>
-    </ProjectsWrapper>
+    </section>
 )
 
 export default Projects
