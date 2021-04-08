7584432bed13cee464f1ca34d75ebcbeb422c397
Isaiah Sherrill <me@isaiahsherrill.com>
2021-04-08 01:26:51 -0500

    Updates

---
+++ component---src-pages-404-js-1a240042119e90152d1d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return c}));var r=t("q1tI"),a=t.n(r),u=t("vrFN");function c(){return a.a.createElement(u.a,null,a.a.createElement("h1",null,"Hey, this is the 404 error page..."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1a240042119e90152d1d.js.map
